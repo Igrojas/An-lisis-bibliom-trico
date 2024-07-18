930d397852527c4257518ef06a782a99420040b4
Igrojas <igrojasro@hotmail.com>
2024-07-18 18:14:34 -0400

    Muchas Cosas

---
+++ grafos.py
@@ -0,0 +1,134 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import streamlit as st
+
+def EjemploDeGrafos():
+    G1 = nx.Graph()
+    G1.add_edges_from([(1, 2), (1, 3), (2, 3), (3, 4)])
+
+    G2 = nx.DiGraph()
+    G2.add_edges_from([(1, 2), (2, 3), (3, 4), (4, 1)])
+
+    G3 = nx.Graph()
+    G3.add_edge(1, 2, weight=4.2)
+    G3.add_edge(2, 3, weight=2.1)
+    G3.add_edge(3, 4, weight=1.8)
+    G3.add_edge(4, 1, weight=3.5)
+
+    G4 = nx.Graph()
+    G4.add_nodes_from([1, 2, 3], bipartite=0)
+    G4.add_nodes_from([4, 5, 6], bipartite=1)
+    G4.add_edges_from([(1, 4), (2, 5), (3, 6), (1, 5), (2, 6)])
+    l, r = nx.bipartite.sets(G4)
+    pos1 = {}
+
+    pos1.update((node, (1, index)) for index, node in enumerate(l))
+    pos1.update((node, (2, index)) for index, node in enumerate(r))
+
+
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+
+    nx.draw(G1, ax=axs[0, 0], with_labels=True, node_color='lightblue', font_weight='bold')
+    axs[0, 0].set_title('Grafo No Dirigido')
+
+    nx.draw(G2, ax=axs[0, 1], with_labels=True, node_color='lightgreen', font_weight='bold', arrows=True)
+    axs[0, 1].set_title('Grafo Dirigido')
+
+    pos = nx.spring_layout(G3)
+    nx.draw(G3, pos, ax=axs[1, 0], with_labels=True, node_color='lightcoral', font_weight='bold')
+    edge_labels = nx.get_edge_attributes(G3, 'weight')
+    nx.draw_networkx_edge_labels(G3, pos, edge_labels=edge_labels, ax=axs[1, 0])
+    axs[1, 0].set_title('Grafo Ponderado')
+
+
+    nx.draw(G4, pos=pos1, ax=axs[1, 1], with_labels=True, node_color='lightyellow', font_weight='bold')
+
+    axs[1, 1].set_title('Grafo Bipartito')
+
+    plt.tight_layout()
+    st.pyplot(plt)
+
+    return
+
+def GrafoCentralidad():
+    
+    G = nx.Graph()
+
+    G.add_edges_from([(1,2),(1,3),(1,4),(3,4),(2,4)])
+    G.add_edges_from([(5,7), (5,6), (7,8), (6,8), (6,9), (8,9)])
+    G.add_edges_from([(10,11), (10,12), (10,14), (11,12), (11,14), (11,13), (12,13)])
+
+    G.add_edges_from([(3,10),(4,14),(2,14),(5,14),(7,14),(7,11),
+                    (6,7),(2,6),(6,13)])
+
+    fig, ax = plt.subplots()
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G,pos=pos, with_labels=True, node_color='lightblue', ax=ax)
+    ax.set_title('Grafo de Ejemplo', fontsize=16)
+
+    st.pyplot(fig)
+    return 
+
+def GrafoEjemplo():
+    G = nx.Graph()
+
+    G.add_edges_from([(1,2),(1,3),(1,4),(3,4),(2,4)])
+    G.add_edges_from([(5,7), (5,6), (7,8), (6,8), (6,9), (8,9)])
+    G.add_edges_from([(10,11), (10,12), (10,14), (11,12), (11,14), (11,13), (12,13)])
+
+    G.add_edges_from([(3,10),(4,14),(2,14),(5,14),(7,14),(7,11),
+                    (6,7),(2,6),(6,13)])
+    return G
+
+
+fig, axs = plt.subplots(1, 3, figsize=(30, 10))
+def PlotCentralidad(G, ax, centrality, title, label):
+
+    centrality_values = list(centrality.values())
+    max_centrality = max(centrality_values)
+    min_centrality = min(centrality_values)
+    normalized_centrality = {node: (value - min_centrality) / (max_centrality - min_centrality) for node, value in centrality.items()}
+
+    colors = [normalized_centrality[node] for node in G.nodes()]
+
+    pos = nx.spring_layout(G, seed=42)
+    nx.draw(G, pos=pos, with_labels=True, node_color=colors, cmap=plt.cm.inferno, ax=ax)
+
+    sm = plt.cm.ScalarMappable(cmap=plt.cm.inferno, norm=plt.Normalize(vmin=min_centrality, vmax=max_centrality))
+    sm.set_array([])
+    fig.colorbar(sm, ax=ax, orientation='vertical', label=label)
+    ax.set_title(title, fontsize=16)
+
+
+def GrafoDirigidoEjemplo():
+    G = nx.DiGraph()
+
+    # Agregar aristas al grafo dirigido
+    G.add_edges_from([(1,2),(1,3),(1,4),(3,4),(2,4)])
+    G.add_edges_from([(5,7), (5,6), (7,8), (6,8), (6,9), (8,9)])
+    G.add_edges_from([(10,11), (10,12), (10,14), (11,12), (11,14), (11,13), (12,13)])
+
+    # Agregar aristas adicionales
+    G.add_edges_from([(3,10),(4,14),(2,14),(5,14),(7,14),(7,11),
+                    (6,7),(2,6),(6,13)])
+    
+    return G
+
+fig, ax = plt.subplots()
+
+def PlotPagerank(G, centrality):
+    centrality_values = list(centrality.values())
+    max_centrality = max(centrality_values)
+    min_centrality = min(centrality_values)
+    normalized_centrality = {node: (value - min_centrality) / (max_centrality - min_centrality) for node, value in centrality.items()}
+
+    colors = [normalized_centrality[node] for node in G.nodes()]
+
+    pos = nx.nx_agraph.graphviz_layout(G, prog='dot')
+    nx.draw(G, pos=pos, with_labels=True, node_color=colors, cmap=plt.cm.inferno, ax=ax)
+
+    sm = plt.cm.ScalarMappable(cmap=plt.cm.inferno, norm=plt.Normalize(vmin=min_centrality, vmax=max_centrality))
+    sm.set_array([])
+    fig.colorbar(sm, orientation='vertical', ax=ax)
+    ax.set_title("PageRank", fontsize=16)
+    st.pyplot(fig)
--- parte_2_app.py
+++ parte_2_app.py
@@ -4,47 +4,198 @@ from PIL import Image
 import streamlit.components.v1 as components
 import matplotlib.pyplot as plt
 import networkx as nx
+from grafos import *
 
-st.title("Análisis de Redes de Coautoría en Psicología Chilena")
+st.title("""Análisis de Redes de Sociales""")
+st.header("Redes de Coautoría")
+
+st.markdown(""" 
+Cuando analizamos redes sociales, es interesante ver que nodos son más "importantes" en una red, esto es responder a las preguntas:
+- ¿Cuales son los nodos con mas conexiones en al red?
+- ¿Por cual nodo tengo que pasar para llegar de un nodo a otro pasando por la menor cantidad de nodos?
+- ¿Cual es el nodo que poseé los caminos mas cortos para llegar a otros nodos, es decir el nodo mas cercano a los otros nodos?
+            
+Estas 3 respuestas tiene un nombre, **CENTRALIDAD**, que para cada respuesta en especifico nos encontramos con:
+- Centralidad de Grado
+- Centralidad de intermediacón
+- Centralidad de cercanía
+            
+Por lo que para aplicar estos conceptos, necesitamos saber a que estructura matemática son aplicables, para ello veamos que es un **GRAFO**
+              """)
 
 st.header("¿Qué es un Grafo?")
 
 st.markdown("""
 Un grafo es una estructura matemática que consiste en un conjunto de **vértices** (nodos) y **aristas** (enlaces) que los conectan. 
-Se representa matemáticamente como \( G = (V, E) \), donde:
-- \( V \) es un conjunto de vértices.
-- \( E \) es un conjunto de aristas, que son pares de vértices que establecen una relación entre ellos.
+Se representa matemáticamente como $G = (V, E) $, donde:
+- $V$ es un conjunto de vértices.            
+- $E$ es un conjunto de aristas, que son pares de vértices que establecen una relación entre ellos.
 
-### Tipos de Grafos:
+### Hay muchos tipos de grafos:
 1. **Grafo No Dirigido**: Las aristas no tienen dirección, es decir, la relación entre dos vértices es simétrica.
 2. **Grafo Dirigido**: Las aristas tienen dirección, indicando que la relación entre dos vértices es unidireccional.
 3. **Grafo Ponderado**: Las aristas tienen un peso o valor asociado que representa alguna medida entre los vértices.
 4. **Grafo Bipartito**: Los vértices se dividen en dos conjuntos disjuntos, y las aristas solo conectan vértices de diferentes conjuntos.
 
-### Ejemplo de Grafo:
+Para el caso de una red social, podemos considerar un grafo no dirigido, o un grafo dirigido, otro grafo ponderado o el grafo bipartito,
+todo dependerá del tipo de problemas que estemos analizando.
+            
+### Ejemplos de cada uno de los grafos:
 """)
 
-G = nx.Graph()
+EjemploDeGrafos()
 
-G.add_node(1)
-G.add_node(2)
-G.add_node(3)
-G.add_node(4)
 
-G.add_edge(1, 2)
-G.add_edge(2, 3)
-G.add_edge(3, 4)
-G.add_edge(4, 1)
-G.add_edge(1, 3)
 
-nx.draw(G, with_labels=True, node_color='blue', node_size=700, font_size=15, font_color='white', edge_color='gray')
-
-# Mostrar el grafo
-st.pyplot(plt)
 
 st.markdown("""
 Los grafos son una herramienta fundamental en la teoría de redes y tienen aplicaciones extensas en diversas áreas del conocimiento y la tecnología.
 """)
+
+
+st.header("Apliquemos las centralidades")
+
+st.markdown("""
+Consideremos el siguiente grafo no dirido y respondamos a las 3 preguntas que teniamos el inicio
+- ¿Cuales son los nodos con mas conexiones en al red?
+- ¿Por cual nodo tengo que pasar para llegar de un nodo a otro pasando por la menor cantidad de nodos?
+- ¿Cual es el nodo que poseé los caminos mas cortos para llegar a otros nodos, es decir el nodo mas cercano a los otros nodos?
+            """)
+G = GrafoCentralidad()
+
+st.markdown("""
+Primero debemos conocer que es cada centralidad y como se calculan 
+
+**Centralidad de Grado:** La centralidad de grado mide la cantidad de conexiones directas que tiene un nodo en una red.
+Si eligo un nodo al azar del grafo, es muy probable que este nodo este conectado con el nodo con mayor centralidad de grado.
+
+Lo único que necesitamos para saber la centralidad de grado de un nodo es cuantas aristas tiene ese nodo, entonces diremos que
+$grado(v)$ va ser el total de aristas de un nodo $v$, y para todo nodo $u \in V$
+""")
+
+st.latex(r''' 
+C_D(u) = \text{grado}(u) 
+''')
+
+st.markdown("""
+**Centralidad de Intermediación:** La centralidad de intermediación mide cuántas veces un nodo actúa como un puente a lo largo del camino más corto entre dos otros nodos.
+Estos nodos con alta centralidad, son los que sostienen la red unida, si eliminamos ese nodo, perdemos muchos caminos que unen otros nodos.
+""")
+
+st.latex(r'''
+C_B(u) = \sum_{s \neq v \neq t} \frac{\sigma_{st}(u)}{\sigma_{st}}
+''')
+
+st.markdown("""
+Donde $\sigma_{st}$ son todos los caminos que unen al nodo $s$ con el nodo $t$, y $\sigma_{st}(u)$ son todos los caminos que pasan por $u$
+""")
+
+st.markdown("""
+**Centralidad de Cercanía:** La centralidad de cercanía mide cuán cerca está un nodo de todos los demás nodos en la red. 
+Este nodo es capaz de llegar a todos los nodos en más rápido que cualquier otro nodo.
+""")
+st.latex(r'''
+C_C(u) = \frac{1}{\sum_{v \in V} d(u, v)}
+''')
+
+st.markdown("""
+Donde $d(u,v)$ es la distancia más corta entre los nodos $u$ y $v$, y $V$ es el conjunto de todos los nodos del grafo $G$.
+""")
+
+st.markdown("""
+Ahora que ya sabemos que es cada centralidad y como calcular cada centralidad, usamos la libreria netwrokX que además de permitir
+dibujar los grafo, ofrece las funciones para calcular las 3 centralidades.
+            
+Una vez que se calculan, se puede representar la centralidad del nodo a través del tamaño del nodo, donde el nodo mas grande es el nodo mas central,
+tambien podemos identificar el valor de cada centralidad a través de colores, que es como se presenta en la siguiente figura.
+""")
+
+
+G = GrafoEjemplo()
+fig, axs = plt.subplots(1, 3, figsize=(30, 10))
+# Centralidad de grado
+centrality = nx.degree_centrality(G)
+PlotCentralidad(G, axs[0], centrality, 'Centralidad de Grado', 'Centralidad de Grado')
+
+# Centralidad de intermediación
+centrality = nx.betweenness_centrality(G)
+PlotCentralidad(G, axs[1], centrality, 'Centralidad de Intermediación', 'Centralidad de Intermediación')
+
+# Centralidad de cercanía
+centrality = nx.closeness_centrality(G)
+PlotCentralidad(G, axs[2], centrality, 'Centralidad de Cercanía', 'Centralidad de Cercanía')
+
+# Ajustar los layouts para que no se sobrepongan
+plt.tight_layout()
+st.pyplot(fig)
+
+st.markdown("""
+El primer grafo de la imágen corresponde a la centralidad de grado, donde los nodos mas centrales fueron el nodo $14$ y el nodo $6$, estos son los nodos
+con mayor aristas en la red.
+            
+En el segundo grafo, que corresponde a la centralidad de intermediacón, vemos que el nodo por donde pasan la mayor cantidad de caminos más cortos es el nodo $6$, 
+que de acuerdo a la red, si queremos llegar de un nodo a otro, es muy probable que debamos pasar por el nodo $6$.
+            
+El último grafo, que corresponde a la centralidad de cercanía, vemos nuevamente al nodo $14$, lo que indica que este nodo $14$ tiene los caminos más cortos para llegar a cualquier otro nodo
+del grafo, seguido del nodo $6$, que tambien es muy cercano a los otros nodos, pero en su caso, tendría que recorrer algunas aristas más.
+""")
+
+st.header("Grafo Dirigido")
+
+st.markdown("""
+¿Que pasa ahora si el grafo fuera digirido?, ¿Importa de donde que nodo sale una aristas?, ¿Importa a que nodo llega una arista?. Para responder estas preguntas,
+exsite un algoritmo llamado **PageRank**.
+            
+PageRank es un algoritmo originalmente creado por Google para su motor de
+búsqueda. Se basa en la idea de que el valor de una página web se puede medir en
+función del número y calidad de los hipervínculos que apuntan hacia ella.
+""")
+
+G = GrafoDirigidoEjemplo()
+fig, ax = plt.subplots()
+pos = nx.nx_agraph.graphviz_layout(G, prog='dot')
+nx.draw(G, pos= pos,  with_labels=True, node_color='lightblue', ax=ax)
+ax.set_title('Grafo Dirigido de Ejemplo', fontsize=16)
+st.pyplot(fig)
+
+st.markdown("""
+Viendo este grafo, ¿Que nodo sería considerado el más importante?, para esto aplicamos el algoritmo de PageRank, que por suerte, la librería Networkx, nos ofrece este algoritmo,        
+""")
+pagerank = nx.pagerank(G)
+PlotPagerank(G,pagerank)
+
+st.markdown("""
+De acuerdo con el algoritmo de PageRank, el nodo mas "importante" de la red es el nodo $14$, el cual es el nodo que otros nodos "importantes" apuntan,
+seguido del nodo $13$ y el nodo $9$""")
+
+st.header("Entonces, de que va mi memoria de título")
+
+st.markdown("""
+Cuando teniamos todos los datos sobre autores y articulos científicos, los mismos que fueron mostrados en la parte 1, queriamos armar una red de coautoría, con 
+los autores como **nodos** y la coautoría como **arista**, pero aplicar centralidad de grado o centralidad de cercanía era algo muy simple de hacer, considerando la cantidad de datos disponibles.
+            
+Entonces podiamos aplicar PageRank, pero tambien nos dejaba con gusto a poco, queriamos aprovechar que teniamos datos como el número de artículos, todas las coautorías de los artículos disponibles,
+la cantidad de coautoía entre autores, etc.
+            
+Aquí es cuando aparece paper [Co-authorship networks in the digital library research community](https://www.sciencedirect.com/science/article/abs/pii/S0306457305000336)
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 st.header("Análisis de la Estructura del Grafo de Coautoría en Psicología")
 st.markdown("""
